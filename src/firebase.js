d6737d66c22317c6309c266cb1f8dadf83988d37
BR-Jayanth <jayanthbychana@gmail.com>
2024-05-22 10:42:06 +0530

    deployment

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore,collection } from "firebase/firestore"
+import { getFirestore, collection } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0b6stzuvxR3qB20Gob5j1d5mDJTK1rrM",
-  authDomain: "mark-down-notes-45630.firebaseapp.com",
-  projectId: "mark-down-notes-45630",
-  storageBucket: "mark-down-notes-45630.appspot.com",
-  messagingSenderId: "665160181826",
-  appId: "1:665160181826:web:6d71dbd878b8e1f48ce5db"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
